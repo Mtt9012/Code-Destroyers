404c724d550ed8a5c6ed11ab5576443c18fded04
Mtt9012 <159050686+Mtt9012@users.noreply.github.com>
2024-02-17 22:53:41 +0100

    In-Dev version 1
    Better input function.
    Working starting menu.

--- CodeDest.c
+++ CodeDest.c
@@ -1,26 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <time.h>
 
-// Tool to clear buffer
+// Tool to clear stdin buffer
 #define CLEARBUFFER while(getchar() != '\n')
+#define CLEARSCREEN printf("\e[1;1H\e[2J")
+
+// Global variables
+unsigned short int numberOfAttempts, numberOfNumbers, sizeOfNumberScope, userInput;
+
 
 // Modular functions
-int InputFunc(){
+int intInputFunc(){
+    printf("\n> ");
     int input;
-    while (true)
-    {
-        printf("> ");
-        if(scanf("%d", &input)){
-            return input;
-        }
+    if(scanf("%d", &input) && input > 0){
+        return input;
+    } else {
         CLEARBUFFER;
-        printf("ILLEGAL INPUT!\n");
+        return -1;
     }
 }
 
+// Starting screen function
+int startingScreenFunction(){
+    start:
+    CLEARSCREEN;
+    printf("+----------# CODE DESTROYERS IN DEV #----------+\n+----------# A CODE DECRYPTION GAME #----------+\n************************************************");
+    printf("\nWRITE:\n1 TO ENTER GAME\n2 TO LEAVE GAME");
+    userInput = intInputFunc();
+    switch (userInput)
+    {
+    case 1:
+        printf("STILL IN DEVELOPMENT!");
+        return 0;
+    case 2:
+        printf("THANK YOU FOR PLAYING!");
+        return 0;
+    default:
+        break;
+    }
+    goto start;
+}
 
 // Starting function
 int main(){
-    return 0;
+    startingScreenFunction();
 }
